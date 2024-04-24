c9f666973c126206c47197c81aed277e18bd8f4f
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-24 17:55:48 -0500

    wip

--- packages/flakka_internal/test/src/flakka_internal_test.dart
+++ packages/flakka_internal/test/src/flakka_internal_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
 import 'package:flakka_internal/flakka_internal.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('FlakkaInternal', () {
