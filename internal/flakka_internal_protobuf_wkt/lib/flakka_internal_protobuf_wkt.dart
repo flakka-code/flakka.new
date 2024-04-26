e0a1113660bd6119124b28caa74143ab49442303
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-26 04:21:20 -0500

    chore: add internal protobuf-wkt package

---
+++ internal/flakka_internal_protobuf_wkt/lib/flakka_internal_protobuf_wkt.dart
@@ -0,0 +1,4 @@
+/// Internal - compiled google/protobuf
+library;
+
+export 'src/flakka_internal_protobuf_wkt.dart';
---
+++ internal/flakka_internal_protobuf_wkt/test/src/flakka_internal_protobuf_wkt_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flakka_internal_protobuf_wkt/flakka_internal_protobuf_wkt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlakkaInternalProtobufWkt', () {
+    test('can be instantiated', () {
+      expect(FlakkaInternalProtobufWkt(), isNotNull);
+    });
+  });
+}
