4a059ceb7a9e3a7e202a435b2dfd53e1f03f2706
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-26 02:24:46 -0500

    chore: cleanup

--- packages/flakka_cli/lib/src/command_runner.dart
+++ packages/flakka_cli/lib/src/command_runner.dart
@@ -1,10 +1,10 @@
 import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:cli_completion/cli_completion.dart';
-import 'package:flakka_cli/src/commands/commands.dart';
-import 'package:flakka_cli/src/version.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:pub_updater/pub_updater.dart';
+import 'package:flakka_cli/src/commands/commands.dart';
+import 'package:flakka_cli/src/version.dart';
 
 const executableName = 'flakka_cli';
 const packageName = 'flakka_cli';
